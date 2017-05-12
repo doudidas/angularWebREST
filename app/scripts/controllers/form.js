5929fe713291fc0d11365f62346e18e05f8029e6
Edouard <edouard.topin@telecom-sudparis.eu>
2017-05-12 18:18:38 +0200

    Initial commit

---
+++ app/scripts/controllers/form.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name activitiApp.controller:FormCtrl
+ * @description
+ * # FormCtrl
+ * Controller of the activitiApp
+ */
+angular.module('activitiApp')
+  .controller('FormCtrl', function () {
+  });
